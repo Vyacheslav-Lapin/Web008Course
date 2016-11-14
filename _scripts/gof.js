83079df97bac77bde9c8ed7c588c49770321723b
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-11-14 12:21:31 +0300

    iterator - 1

--- _scripts/gof.js
+++ _scripts/gof.js
@@ -12,12 +12,13 @@ const a = {
 a.method1();
 a.method2();
 
-const B = (function() {
+const B = (function () {
 
     class B {
         constructor() {
             this.field3 = 50;
         }
+
         method3(x) {
             return this.field3 + x;
         }
@@ -25,7 +26,7 @@ const B = (function() {
 
     let b;
 
-    return function() {
+    return function () {
         if (b === undefined)
             b = new B();
         return b;
@@ -34,46 +35,78 @@ const B = (function() {
 })();
 
 const b = new B();
-const c = new B();
+const b1 = new B();
 console.log(b.method3(5));
 // ...
-console.log(b === c); // true
+console.log(b === b1); // true
 
-class C {
-    constructor() {
-        this.field1 = 3;
-        this.field2 = 5;
-    }
+const C = (() => {
 
-    method() {
-        return this.field1 + this.field2;
-    }
+    let constructorAvailable = false;
 
-    /**
-     * @param {C} c
-     * @return {boolean}
-     */
-    equals(c) {
-        return this.field1 === c.field1 &&
+    class C {
+        constructor() {
+
+            if (!constructorAvailable)
+                throw "Ошибка! Попытка вызвать извне приватный конструктор!";
+
+            this.field1 = 3;
+            this.field2 = 5;
+        }
+
+        method() {
+            return this.field1 + this.field2;
+        }
+
+        /**
+         * @param {C} c
+         * @return {boolean}
+         */
+        equals(c) {
+            return this.field1 === c.field1 &&
                 this.field2 === c.field2 &&
                 this.method() === c.method();
+        }
+
+        static getC() {
+            constructorAvailable = true;
+            try {
+                return new D();
+            } finally {
+                constructorAvailable = false;
+            }
+        }
     }
 
-    static getC() {
-        return new D();
+    class D extends C {
+        method2() {
+            return 10 + this.method();
+        }
     }
-}
 
-class D extends C {
-    method2() {
-        return 10 + this.method();
-    }
-}
+    return C;
+})();
 
-const c = new C();
+const c = C.getC();
 const c1 = C.getC();
 
 console.log(c === c1); // false
 console.log(c.equals(c1)); // true
 console.log(c instanceof C); // true
 console.log(c1 instanceof C); //true
+console.log(C.name); //C
+
+const iterator = {
+    array: [1,2,3],
+    index: 0,
+    next: function () {
+        return this.array[this.index++];
+    },
+    // remove: function () {
+    //
+    // }
+};
+
+let iteratorElement;
+while ((iteratorElement = iterator.next()) !== undefined)
+    console.log(iteratorElement); // 1, 2, 3
