1d7365de92aeb085328a829a5855040e6bab6284
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-11-17 12:25:48 +0300

    DOM Events - 2

--- _scripts/eventListeners.js
+++ _scripts/eventListeners.js
@@ -1,13 +1,22 @@
+"use strict";
+
 // не самое оптимальное решение
 const clickMe = document.getElementById(`clickMe`),
-count = 0;
+    clickWrap = document.getElementById(`click-wrap`);
 
 clickMe.addEventListener(`click`, myHandler, false);
+clickWrap.addEventListener(`click`, myHandler, false);
 
 function myHandler(evt) {
-    const src = evt.target.getElementsByClassName(`counter`)[0];
-    src.innerHTML = parseInt(src.innerText) + 1;
+    const node = getCounter(evt);
+    node.innerHTML = parseInt(node.innerText) + 1;
 
     evt.stopPropagation(); // предотвратить дальнейшее всплытие события
     evt.preventDefault(); // предотвратить выполнение действия по умолчанию
+
+    function getCounter(evt) {
+        const counter1 = evt.target,
+            counter2 = counter1.getElementsByClassName(`counter`)[0];
+        return counter2 ? counter2 : counter1;
+    }
 }
